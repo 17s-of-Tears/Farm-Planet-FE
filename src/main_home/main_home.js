35630266f388f035ade9515291098da3701a4f73
BoungChan Song <thdqudcks2@gmail.com>
2021-11-10 23:22:04 +0900

    메인 css 수정중

--- src/main_home/main_home.js
+++ src/main_home/main_home.js
@@ -3,7 +3,8 @@ import { Input } from 'antd';
 import 'antd/dist/antd.css';
 
 import './main_home.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/css_noamlfont.css';
+
 
 const MainHome = (props) => {
   
@@ -22,20 +23,54 @@ const MainHome = (props) => {
           </div>
         </div>
       </div>
+
       <div className="mainBox">
-        <div className="content_item">
-          <div className="imageBox"><img src='img/main_content_img1.png'></img></div>
-          <p className="textLine">자연친화적인 작물을 가족들과 혹은 혼자서 즐겨보세요. 언제나 최선을 다해 서비스를 제공합니다.</p>
-        </div>  
-        <div className="content_item">
-          <div className="imageBox"><img src='img/main_content_img2.png'></img></div>
-          <p className="textLine">언제나 원할때는 주말농장으로 사용하실 수 있습니다.</p>
+        
+        <div className="leftContent">
+          <div className="textLine">
+            <h2>Keyword</h2>
+            <h3>소비자 중심의 자율적인 먹거리와 서비스를 제공합니다.</h3>
+            <p>단순히 서비스를 제공하는 것이 아니라<br></br>
+              우리 가족이 먹는다는 생각으로 키우고 관리합니다.</p>
+          </div>
+          <div className="content_item">
+            <div className="imageBox"><img src='img/temp_main_content_img1.png'></img></div>
+            <div className="textBox"> 
+              <h3>직접 키우고 받는 작물</h3>
+              <div className="underLine"></div>             
+              <p>자신이 원하는 작물을 받을 수 있습니다.<br></br>정성을 들여 키운 작물을  직접 재배 및 배송을 받을 수도 있습니다.</p>
+            </div>            
+          </div>
+          <div className="keywordLine">
+            <p>"Delivering happiness</p>
+            <p>Sharing health"</p>
+          </div>
+
         </div>
-        <div className="content_item">
-          <div className="imageBox"><img src='img/main_content_img3.png'></img></div>
-          <p className="textLine">자신이 원하는 작물을 받을 수 있습니다.<br></br>정성을 들여 키운 작물을 직접 재배할 수도 배송을 받을 수도 있습니다</p>
-        </div>       
+
+        <div className="rightContent">
+          <div className="content_item">
+            <div className="imageBox"><img src='img/temp_main_content_img2.png'></img></div>
+            <div className="textBox"> 
+              <h3>주말농장</h3>
+              <div className="underLine"></div>             
+              <p>언제나 원할때는 주말농장으로<br></br>사용하실 수 있습니다.</p>
+            </div>            
+          </div>
+
+          <div className="content_item">
+            <div className="imageBox"><img src='img/temp_main_content_img3.png'></img></div>
+            <div className="textBox"> 
+              <h3>유기농</h3>
+              <div className="underLine"></div>             
+              <p>자연친화적인 작물을 가족들과 혹은 혼자서<br></br>즐겨보세요. 언제나 최선을 다해 서비스를<br></br>제공합니다.</p>
+            </div>            
+          </div>
+
+        </div>
+           
       </div>
+
       <div className="questionBox">'
         <h4>자주하는 질문들</h4>
         <ul>
