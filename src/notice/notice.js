adc26ede691e80bda8fc6590038974260672773b
BoungChan Song <thdqudcks2@gmail.com>
2021-11-17 20:44:43 +0900

    공지사항 완료

--- src/notice/notice.js
+++ src/notice/notice.js
@@ -4,47 +4,77 @@ import './notice.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'antd/dist/antd.css';
-import { Tabs } from 'antd';
+import { Tabs, Input, Pagination } from 'antd';
 
-const Notice = (props) => {
+const Notice = (props) => {  
+  let [currentPage,setCurrentPage] = useState(1); 
+  let [tab_isOn,setTabisOn] = useState(1); 
   
-  const { TabPane } = Tabs;
+  //페이지 네이션 관련 함수
+  const onChangePage = page => {
+    
+    console.log(page);
+    setCurrentPage(page);
+  };
 
+  const { TabPane } = Tabs;
+  const { Search } = Input;
+
+  //검색 관련 함수
+  const onSearch = value => {console.log(value);}
+  // const onSearch = function(value, event){
+  //   console.log(value);
+  // }
   function callback(key) {
     console.log(key);
   }
+  const onClickTab = () => {
+    setTabisOn(tab_isOn == 1 ? 2 : 1)
+  }  
 
   return (
     <div className="Notice">     
-      <div></div>
+      <div className="bannerBox">
+        <div className="imageBox"><img src='img/notice_banner.png' alt="notice_banner"></img></div>         
+        
+        <div className="textBox">
+          <h1>팜 플래닛</h1>
+          <p>서비스 이용관련 문의사항을 해결해 드립니다.</p>
+        </div>
+       
+      </div>
+      <div className="board_contaionr">
+        <div className="tabs">        
+          <div className={"tabs-tab" + (tab_isOn == 1 ? " tabs-tab-active" : "" )} onClick={onClickTab}>공지사항</div>
+          <div className={"tabs-tab" + (tab_isOn == 2 ? " tabs-tab-active" : "" )} onClick={onClickTab}>FAQ</div>        
+        </div>
 
-      <Tabs onChange={callback} type="card">
-        <TabPane tab="공지사항" key="1">
-          <div className="list_containor">
+        <div className="list_containor">
+          <div className="title_line">
             <h3>전체</h3>
-            <div className="list_content">
-              <div className="list_item">
-                <h4>농림식품부 정책 변경에 따른 서비스 이용약관 변경 </h4>
-                <p>2021-11-06</p>
-              </div>
+            <Search placeholder="input search text" allowClear onSearch={onSearch} style={{ width: 300 }}/>
+          </div>
+          
+          <div className="list_content">
+            <div className="list_item">
+              <h4>농림식품부 정책 변경에 따른 서비스 이용약관 변경 </h4>
+              <p>2021-11-06</p>
+            </div>
 
-              <div className="list_item">
-                <h4>업데이트 공지</h4>
-                <p>2021-11-06</p>
-              </div>
-              
-              <div className="list_item">
-                <h4>업데이트 공지</h4>
-                <p>2021-11-06</p>
-              </div>
+            <div className="list_item">
+              <h4>업데이트 공지</h4>
+              <p>2021-11-06</p>
+            </div>
+            
+            <div className="list_item">
+              <h4>업데이트 공지</h4>
+              <p>2021-11-06</p>
             </div>
           </div>
-        </TabPane>
-        <TabPane tab="FAQ" key="2">            
-          b
-        </TabPane>
+        </div>
+      </div>
 
-      </Tabs>
+      <Pagination current={currentPage} total={100} onChange={onChangePage}/>
     </div>
   );
 };
