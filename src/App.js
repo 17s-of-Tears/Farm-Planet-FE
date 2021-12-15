bf5ea71eeccb34b2b300d727e1f6354b45144114
Joseph <notseph8556@gmail.com>
2021-12-15 23:27:45 +0900

    Delete App.js Link

--- src/App.js
+++ src/App.js
@@ -26,20 +26,6 @@ function App() {
 
   return (
     <div className="App">
-      <ul>
-        <li><Link exact to="/">메인</Link></li>
-        <li><Link to="/signup">회원가입</Link></li>
-        <li><Link to="/login">로그인</Link></li>
-        <li><Link to="/profile">프로필</Link></li>
-        <li><Link to="/myplant">내 식물</Link></li>
-        <li><Link to="/detailplant">식물사진</Link></li>
-        <li><Link to="/list">리스트</Link></li>
-        <li><Link to="/customer-center">공지사항</Link></li>
-        <li><Link to="/admin">어드민</Link></li>
-        <li><Link to="/subscript">구독페이지</Link></li>
-        <li><Link to="/profilemap">구독페이지</Link></li>
-        <li><Link to="/profilemyplantimage">구독페이지</Link></li>
-      </ul>
       <Suspense fallback={<div>loading...</div>}>
         <Switch>
           <Route exact path='/'>
